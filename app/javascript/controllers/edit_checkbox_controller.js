77b14c5c63e07d27ad6131777e653e52051a3d89
Matt Eva <matteweva@gmail.com>
2026-05-21 08:31:11 -0700

    populating checkboxes that connect to edit checkbox stimulus controller

--- app/javascript/controllers/edit_checkbox_controller.js
+++ app/javascript/controllers/edit_checkbox_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ["checkbox"];
   connect() {
     console.log("hello stimulus", this.element);
   }
-  checkLimit() {}
+  checkLimit(event) {
+    console.log(event.target);
+  }
 }
