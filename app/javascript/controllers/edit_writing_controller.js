f063b248fad2c2db635032c398c2c7787460aa59
Matt Eva <matteweva@gmail.com>
2026-05-29 09:50:33 -0700

    successfully setting initial values (for prepopulated fields
    successfully reseting title field on cancel

--- app/javascript/controllers/edit_writing_controller.js
+++ app/javascript/controllers/edit_writing_controller.js
@@ -1,15 +1,25 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["checkbox", "tagInput", "tagList", "submit", "cancel"];
+  static targets = [
+    "title",
+    "creator",
+    "writingType",
+    "genreCheckbox",
+    "tagInput",
+    "tagList",
+    "description",
+    "submit",
+    "cancel",
+  ];
 
   static values = {
     initialTitle: String,
-    initialDescription: String,
     initialCreator: String,
     initialWritingType: String,
     initialGenres: Array,
     initialTags: Array,
+    initialDescription: String,
     currentTags: Array,
     checkedBoxes: Number,
   };
@@ -17,7 +27,26 @@ export default class extends Controller {
   initialize() {}
 
   connect() {
-    this.checkboxTargets.forEach((el) => {
+    this.#populateInitialValues();
+  }
+
+  #populateInitialValues() {
+    this.initialTitleValue = this.titleTarget.value;
+    this.initialCreatorValue = this.creatorTarget.value;
+    this.initialWritingTypeValue = this.writingTypeTarget.value;
+    this.#populateInitialGenres();
+    this.#populateInitialTags();
+    this.initialDescriptionValue = this.descriptionTarget.value;
+    console.log(this.initialTitleValue);
+    console.log(this.initialCreatorValue);
+    console.log(this.initialWritingTypeValue);
+    console.log(this.initialGenresValue);
+    console.log(this.initialTagsValue);
+    console.log("initialDescriptionvalue", this.initialDescriptionValue);
+  }
+
+  #populateInitialGenres() {
+    this.genreCheckboxTargets.forEach((el) => {
       if (el.checked) {
         this.checkedBoxesValue++;
         this.initialGenresValue = [...this.initialGenresValue, el.value];
@@ -25,11 +54,18 @@ export default class extends Controller {
     });
 
     if (this.checkedBoxesValue >= 3) {
-      this.checkboxTargets.forEach((el) => {
+      this.genreCheckboxTargets.forEach((el) => {
         if (!el.checked) {
           el.disabled = true;
         }
       });
+    }
+  }
+
+  #populateInitialTags() {
+    const tagSpanTexts = document.getElementsByClassName(".tagSpanText");
+    for (const span of tagSpanTexts) {
+      this.InitialTagsValue = [...this.InitialTagsValue, span.textContent];
     }
   }
 
@@ -39,8 +75,12 @@ export default class extends Controller {
   }
 
   cancelUpdate() {
-    console.log(this.cancelTarget);
+    this.submitTarget.disabled = true;
+    this.cancelTarget.disabled = true;
+    this.titleTarget.value = this.initialTitleValue;
   }
+  #restoreInitialGenres() {}
+  #restoreInitialTags() {}
 
   checkLimit(event) {
     this.registerChange();
@@ -49,7 +89,7 @@ export default class extends Controller {
     }
 
     if (this.checkedBoxesValue >= 3) {
-      for (const el of this.checkboxTargets) {
+      for (const el of this.genreCheckboxTargets) {
         if (!el.checked) {
           el.disabled = true;
         }
@@ -58,7 +98,7 @@ export default class extends Controller {
 
     if (!event.target.checked) {
       if (this.checkedBoxesValue === 3) {
-        for (const el of this.checkboxTargets) {
+        for (const el of this.genreCheckboxTargets) {
           if (el.disabled) {
             el.disabled = false;
           }
--- app/models/writing.rb
+++ app/models/writing.rb
@@ -6,4 +6,5 @@ class Writing < ApplicationRecord
   has_many :chapters, dependent: :destroy
 
   GENRES = ["Action", "Adventure", "Comedy", "Drama", "Erotica", "Fantasy", "Historical Fiction", "Horror", "Literary Fiction", "Magical Realism", "Mystery", "Romance", "Science Fiction", "Slice of Life", "Speculative Fiction", "Social Fiction", "Supernatural", "Superhero", "Thriller", "Young Adult"].freeze
+  WRITING_TYPES = ["Short Story", "Novellete", "Novella", "Novel","Poetry", "Essay", "Blog", "Memoir", ].freeze
 end
