872e4c833806d98681416dc5f5f44ed5b2a7846c
Matt Eva <matteweva@gmail.com>
2026-06-10 04:26:39 -0700

    added language describing writing types to about page

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -5,6 +5,7 @@ class Writing < ApplicationRecord
   belongs_to :user
   has_many :chapters, dependent: :destroy
 
-  GENRES = ["Action", "Adventure", "Comedy", "Drama", "Erotica", "Fantasy", "Historical Fiction", "Horror", "Literary Fiction", "Magical Realism", "Mystery", "Romance", "Science Fiction", "Slice of Life", "Speculative Fiction", "Social Fiction", "Supernatural", "Superhero", "Thriller", "Young Adult"].freeze
-  WRITING_TYPES = ["Short Story", "Novellete", "Novella", "Novel","Poetry", "Essay", "Blog", "Memoir", ].freeze
+  FICTION_GENRES = ["No Genre", "Action", "Adventure", "Comedy", "Drama", "Erotica", "Fantasy", "Historical Fiction", "Horror", "Literary Fiction", "Magical Realism", "Mystery", "Romance", "Science Fiction", "Slice of Life", "Speculative Fiction", "Social Fiction", "Supernatural", "Superhero", "Thriller", "Young Adult"].freeze
+  ESSAY_TOPICS = [].freeze
+  WRITING_TYPES = ["Short Story", "Novellete", "Novella", "Novel", "Poetry", "Essay", "Thought Piece", "Blog", "Memoir"].freeze
 end
