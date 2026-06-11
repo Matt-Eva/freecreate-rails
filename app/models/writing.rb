e503ac24f04c8e3cd91905cd521c305ea2b00aea
Matt Eva <matteweva@gmail.com>
2026-06-10 19:13:57 -0700

    set edit form to correctly render no genre for fiction instead of no topic

--- app/models/writing.rb
+++ app/models/writing.rb
@@ -1,13 +1,13 @@
 class Writing < ApplicationRecord
   validates :title, presence: true
   validates :writing_type, presence: true
-  validates :las_published, presence: true
+  validates :last_published, presence: true
 
   belongs_to :creator
   belongs_to :user
   has_many :chapters, dependent: :destroy
 
-  FICTION_GENRES = ["No Genre", "Action", "Adventure", "Comedy", "Drama", "Erotica", "Fantasy", "Historical Fiction", "Horror", "Literary Fiction", "Magical Realism", "Mystery", "Romance", "Science Fiction", "Slice of Life", "Speculative Fiction", "Social Fiction", "Supernatural", "Superhero", "Thriller", "Young Adult"].freeze
+  FICTION_GENRES = ["No Topic", "Action", "Adventure", "Comedy", "Drama", "Erotica", "Fantasy", "Historical Fiction", "Horror", "Literary Fiction", "Magical Realism", "Mystery", "Romance", "Science Fiction", "Slice of Life", "Speculative Fiction", "Social Fiction", "Supernatural", "Superhero", "Thriller", "Young Adult"].freeze
   ESSAY_TOPICS = [].freeze
   WRITING_TYPES = ["Short Story", "Novellete", "Novella", "Novel", "Poetry", "Essay", "Thought Piece", "Blog", "Memoir"].freeze
 end
